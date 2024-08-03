6b7ae56a449f1eec2fb7a14e7e5c687111e43a10
Malerce <brazdeczkiy.denis@gmail.com>
2024-08-03 20:00:53 +0300

    add audio

--- src/app/[locale]/partners/page.tsx
+++ src/app/[locale]/partners/page.tsx
@@ -6,6 +6,7 @@ const PartnersPage: React.FC = () => {
 
   return (
     <div className=' relative  w-full overflow-hidden bg-side text-white'>
+    
       <Image className=" absolute max-[1023px]:opacity-0 lg:right-[-19%] lg:bottom-[1%] min-[1540px]:right-[-25%] min-[1540px]:bottom-[1%] h-full w-full" src={lines} alt="background"/>
       <Image className=" absolute min-[1023px]:opacity-0 max-[619px]:right-[-5%] max-[619px]:bottom-[8%] max-[1023px]:right-[-15%] max-[1023px]:bottom-[2%] h-full w-full" src={linesMobile} alt="background"/>
       <Partners/>
