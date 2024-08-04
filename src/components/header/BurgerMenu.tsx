731f1fa392c0d721b48e9f49a275dbe3a75fe0b6
Malerce <brazdeczkiy.denis@gmail.com>
2024-08-04 22:24:35 +0300

    fix burger

--- src/components/header/BurgerMenu.tsx
+++ src/components/header/BurgerMenu.tsx
@@ -24,16 +24,21 @@ const BurgerMenu:React.FC<{ isMenuActive: boolean, handleCloseMenu: () => void }
 
 
     return (
-      <div
-        ref={modalMenuRef}
-        className={`fixed flex opacity-90 items-center justify-center z-30 bg-customOrange top-0 right-0 w-[45%] h-screen transition-transform duration-500 transform ${
-          isMenuActive ? 'translate-x-0' : 'translate-x-full'
-        }`}
-      >
-        <div className="flex flex-col items-center relative z-10 justify-center space-y-4 py-10 font-daysOne text-black">
-          <Links />
+      <>
+        
+        <div
+          ref={modalMenuRef}
+          className={`fixed flex opacity-90 items-center justify-center z-30 bg-customOrange top-0 right-0 w-[45%] h-screen transition-transform duration-500 transform ${
+            isMenuActive ? 'translate-x-0' : 'translate-x-full'
+          }`}
+        >
+          <span className="block bg-customOrange w-[100%] h-[9999px] absolute "></span>
+          <div className="flex flex-col items-center relative z-10 justify-center space-y-4 py-10 font-daysOne text-black">
+            <Links />
+          </div>
         </div>
-      </div>
+      </>
+      
     );
   };
   BurgerMenu.displayName = "BurgerMenu";
