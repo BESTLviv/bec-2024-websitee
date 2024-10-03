b3499701a2618cd89da948a9b37dc70376a74bbd
Malerce933 <brazdik14@gmail.com>
2024-10-03 17:59:21 +0300

    fix seo

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://best-lviv.org.ua', // правильний домен вашого сайту
+    generateRobotsTxt: true, // опціонально генерувати robots.txt
+    changefreq: 'daily', // частота перевірки змін сторінки
+    priority: 0.7, // пріоритет індексації сторінок
+}
