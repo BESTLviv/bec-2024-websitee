0fedc89dc9d1b3f12d4c6fc625c6c31cbd62a279
Malerce933 <brazdik14@gmail.com>
2024-10-05 21:41:52 +0300

    add button registration

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,8 @@ module.exports = {
     generateRobotsTxt: true, // опціонально генерувати robots.txt
     changefreq: 'daily', // частота перевірки змін сторінки
     priority: 0.7, // пріоритет індексації сторінок
+    additionalPaths: async (config) => [
+        await config.transform(config, '/partners'), // Додає сторінку /partners
+        await config.transform(config, '/organizers'), // Додає сторінку /organizers
+      ],
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -14,7 +14,9 @@ export default function Header() {
                 <h2>ENGINEERING</h2>
                 <h2>COMPETITION</h2>
                 <h3 className=" font-rubikB font-bold self-center">{t('date')}</h3>
+                <a className=" bg-customOrange font-rubik link__bot flex justify-center self-center items-center text-black hover:scale-[1.1] transition-transform duration-500" href="https://t.me/bec24_bot" target="_blank">{t('registration')}</a>
              </div>
+            
         </header>
     )
 }
