ef8ece342bcf0d6389fe68889c6c0cdfd24edc63
Malerce933 <brazdik14@gmail.com>
2024-11-05 16:23:22 +0200

    change partners

--- src/components/partners/data.ts
+++ src/components/partners/data.ts
@@ -1,15 +1,13 @@
 import { StaticImageData } from "next/image"
-import dataart from "/public/partners/dataart.svg"
-import dialog from "/public/partners/dialog.svg"
+
 import fujikura from "/public/partners/fujikura.svg"
 import globallogic from "/public/partners/globallogic.svg"
-import leobit from "/public/partners/leobit.svg"
-import rozkvit from "/public/partners/rozkvit.svg"
-import sigma from "/public/partners/sigma.svg"
-import squad from "/public/partners/squad.svg"
-import teamvoy from "/public/partners/teamvoy.svg"
-import warbirds from "/public/partners/warbirds.svg"
-import tskt from "/public/partners/tskt.svg"
+
+import tech from "/public/partners/tech.svg"
+import openlab from "/public/partners/openlab.svg"
+import meleto from "/public/partners/meleto.svg"
+import best from "/public/partners/best.svg"
+import kness from "/public/partners/kness.svg"
 import softserve from "/public/partners/softserve.svg"
 import renesans from "/public/partners/renesans.svg"
 import hator from "/public/partners/hator.svg"
@@ -17,15 +15,18 @@ import hamily from "/public/partners/hamily.svg"
 import ilovekebab from "/public/partners/ilovekebab.svg"
 
 const partners: StaticImageData[] = [
-    warbirds,
+    kness,
     renesans,
     fujikura,
-    sigma,
+    best,
     softserve,
     globallogic,
+    ilovekebab,
     hator,
     hamily,
-    ilovekebab
+    meleto,
+    openlab,
+    tech
 ]
 
 export default partners;
